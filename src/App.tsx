e82c307ff1185e1ee9a916102867cf44b5aa8665
Reactongraph <js.developer@ongraph.com>
2025-05-09 18:49:59 +0530

    route issue fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
-import CaptureNoShow from './pages/CaptureNoShow';
-import CustomerLookup from './pages/CustomerLookup';
-import CustomerDetails from './pages/CustomerDetails';
-import Settings from './pages/Settings';
-import SubscriptionPage from './pages/SubscriptionPage';
-import PaymentPage from './pages/PaymentPage';
-import PricingPage from './pages/PricingPage';
-import SignupPage from './pages/SignupPage';
-import LandingPage from './pages/LandingPage';
-import PaywallWrapper from './components/PaywallWrapper';
-import { NoShowProvider } from './context/NoShowContext';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import CaptureNoShow from "./pages/CaptureNoShow";
+import CustomerLookup from "./pages/CustomerLookup";
+import CustomerDetails from "./pages/CustomerDetails";
+import Settings from "./pages/Settings";
+import SubscriptionPage from "./pages/SubscriptionPage";
+import PaymentPage from "./pages/PaymentPage";
+import PricingPage from "./pages/PricingPage";
+import SignupPage from "./pages/SignupPage";
+import LandingPage from "./pages/LandingPage";
+import PaywallWrapper from "./components/PaywallWrapper";
+import { NoShowProvider } from "./context/NoShowContext";
 
 function App() {
   return (
@@ -23,11 +28,39 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/app" element={<Layout />}>
-            <Route index element={<PaywallWrapper><Dashboard /></PaywallWrapper>} />
-            <Route path="capture" element={<PaywallWrapper><CaptureNoShow /></PaywallWrapper>} />
-            <Route path="lookup" element={<PaywallWrapper><CustomerLookup /></PaywallWrapper>} />
-            <Route path="customer/:phoneNumber" element={<PaywallWrapper><CustomerDetails /></PaywallWrapper>} />
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <PaywallWrapper>
+                  <Dashboard />
+                </PaywallWrapper>
+              }
+            />
+            <Route
+              path="capture"
+              element={
+                <PaywallWrapper>
+                  <CaptureNoShow />
+                </PaywallWrapper>
+              }
+            />
+            <Route
+              path="lookup"
+              element={
+                <PaywallWrapper>
+                  <CustomerLookup />
+                </PaywallWrapper>
+              }
+            />
+            <Route
+              path="customer/:phoneNumber"
+              element={
+                <PaywallWrapper>
+                  <CustomerDetails />
+                </PaywallWrapper>
+              }
+            />
             <Route path="settings" element={<Settings />} />
             <Route path="pricing" element={<PricingPage />} />
             <Route path="settings/billing" element={<SubscriptionPage />} />
